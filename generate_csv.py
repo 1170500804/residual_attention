cbb55be4adda9c0a2f3236e8542eada38904bbdb
Shuai Liu <1170500804hit@gmail.com>
2020-05-30 19:57:38 +0800

    description

--- generate_csv.py
+++ generate_csv.py
@@ -1,10 +1,21 @@
 import pandas as pd
 import os
-df = pd.DataFrame(columns=['dir', 'class'])
-root_dir = '/home/liushuai/cleaned_images/train'
-for d in range(1,7):
-    cur_cat = str(d+5000)
-    cur_dir = os.path.join(root_dir, cur_cat)
-    for l in os.listdir(cur_dir):
-        df.loc[len(df)] = [os.path.join(cur_dir, l), cur_cat]
-df.to_csv(os.path.join(root_dir, 'description.csv'))
+def generate(root_dir, is_train=True):
+    df = pd.DataFrame(columns=['dir', 'class'])
+    if(is_train):
+        for d in range(1,7):
+            cur_cat = str(d+5000)
+            cur_dir = os.path.join(root_dir, cur_cat)
+            for l in os.listdir(cur_dir):
+                df.loc[len(df)] = [os.path.join(cur_dir, l), cur_cat]
+        df.to_csv(os.path.join(root_dir, 'description_train.csv'))
+    else:
+        for d in range(1,7):
+            cur_cat = str(d+5000)
+            cur_dir = os.path.join(root_dir, cur_cat)
+            for l in os.listdir(cur_dir):
+                df.loc[len(df)] = [os.path.join(cur_dir, l), cur_cat]
+        df.to_csv(os.path.join(root_dir, 'description_test.csv'))
+
+generate('/home/liushuai/cleaned_images/train')
+generate('/home/liushuai/cleaned_images/validate', is_train=False)
