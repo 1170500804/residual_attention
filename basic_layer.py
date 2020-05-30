442026c84830ee27ba16526cb41a3736406ec376
Shuai Liu <1170500804hit@gmail.com>
2020-05-30 21:02:18 +0800

    description

--- basic_layer.py
+++ basic_layer.py
@@ -1,34 +1,40 @@
 import torch
 import torch.nn as nn
+from torch.nn import init
+import functools
+from torch.autograd import Variable
+import numpy as np
 
-class Residual_Block(nn.Module):
-    def __init__(self, in_channel, out_channel, stride=1):
-        super(Residual_Block, self).__init__()
-        self.in_channel = in_channel
-        self.out_channel = out_channel
+
+class ResidualBlock(nn.Module):
+    def __init__(self, input_channels, output_channels, stride=1):
+        super(ResidualBlock, self).__init__()
+        self.input_channels = input_channels
+        self.output_channels = output_channels
         self.stride = stride
-        self.bn1 = nn.BatchNorm2d(in_channel)
+        self.bn1 = nn.BatchNorm2d(input_channels)
         self.relu = nn.ReLU(inplace=True)
-        self.conv1 = nn.Conv2d(in_channel, out_channel/4, 1,stride=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_channel/4)
-        self.conv2 = nn.Conv2d(out_channel / 4, out_channel / 4, 3, stride=stride, bias=False)
-        self.bn3 = nn.BatchNorm2d(out_channel/4)
-        self.conv3 = nn.Conv2d(out_channel / 4, out_channel, 1, stride=1, bias=False)
-        self.conv4 = nn.Conv2d(in_channel, out_channel, 1, stride=stride, bias=False)
-    def forward(self, x):
-        res = x
-        x = self.bn1(x)
-        out_1 = self.relu(x)
-        x = out_1
-        x = self.conv1(x)
-        x = self.bn2(x)
-        x = self.relu(x)
-        x = self.conv2(x)
-        x = self.bn3(x)
-        x = self.relu(x)
-        x = self.conv3(x)
-        if(self.in_channel != self.out_channel) or (self.stride!=1):
-            res = self.conv4(out_1)
-        x += res
-        return x
+        self.conv1 = nn.Conv2d(input_channels, output_channels / 4, 1, 1, bias=False)
+        self.bn2 = nn.BatchNorm2d(output_channels / 4)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(output_channels / 4, output_channels / 4, 3, stride, padding=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(output_channels / 4)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv3 = nn.Conv2d(output_channels / 4, output_channels, 1, 1, bias=False)
+        self.conv4 = nn.Conv2d(input_channels, output_channels, 1, stride, bias=False)
 
+    def forward(self, x):
+        residual = x
+        out = self.bn1(x)
+        out1 = self.relu(out)
+        out = self.conv1(out1)
+        out = self.bn2(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn3(out)
+        out = self.relu(out)
+        out = self.conv3(out)
+        if (self.input_channels != self.output_channels) or (self.stride != 1):
+            residual = self.conv4(out1)
+        out += residual
+        return out
